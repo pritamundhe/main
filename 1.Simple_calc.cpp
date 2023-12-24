59fd09bb73cc5dd6ef5de2345f41f519bef7a13e
Pritam Mundhe <pritammundhe00@gmail.com>
2023-12-24 13:46:42 +0530

    Initial commit

---
+++ 1.Simple_calc.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter value for A : ";
+    cin>>a;
+    cout<<"Enter value for B : ";
+    cin>>b;
+    //addition
+    cout<<"Addition is : "<<a+b;
+    cout<<"Subtraction is : "<<a-b;
+    cout<<"Multiplication is : "<<a*b;
+    cout<<"Division is : "<<a/b;
+    cout<<"Modules is : "<<a%b;
+    return 0;
+}
